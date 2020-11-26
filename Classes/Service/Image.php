262864a821437abdbfca67b5439823bb848b0171
Benjamin Hirsch <mail@benjaminhirsch.net>
2020-11-26 20:57:01 +0100

    Removed support for non composer mode. Fixes #10

--- Classes/Service/Image.php
+++ Classes/Service/Image.php
@@ -16,17 +16,32 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 (static function () {
 
-    if (file_exists(dirname(__DIR__, 5) . '/typo3_src/vendor/autoload.php')) {
-        // Non composer mode
-        $classLoader = require dirname(__DIR__, 5) . '/typo3_src/vendor/autoload.php';
-        SystemEnvironmentBuilder::run(5, SystemEnvironmentBuilder::REQUESTTYPE_FE);
-        Bootstrap::init($classLoader);
-    } else {
+    // Default vendor dir
+    if (file_exists(dirname(__DIR__, 6) . '/vendor/autoload.php')) {
         // Composer mode
         $classLoader = require dirname(__DIR__, 6) . '/vendor/autoload.php';
         SystemEnvironmentBuilder::run(5, SystemEnvironmentBuilder::REQUESTTYPE_FE);
         Bootstrap::init($classLoader);
+
+    // Custom vendor dir
+    } else if (isset($_SERVER['DOCUMENT_ROOT']) &&
+        file_exists(dirname($_SERVER['DOCUMENT_ROOT'], 1) . '/composer.json')) {
+        $composerConfig = json_decode(file_get_contents(
+            dirname($_SERVER['DOCUMENT_ROOT'], 1) . '/composer.json'
+        ), true, 512, JSON_THROW_ON_ERROR);
+
+        if (isset($composerConfig['config']['vendor-dir'])) {
+            $classLoader = require dirname(__DIR__, 6) . '/' . $composerConfig['config']['vendor-dir'] . '/autoload.php';
+            SystemEnvironmentBuilder::run(5, SystemEnvironmentBuilder::REQUESTTYPE_FE);
+            Bootstrap::init($classLoader);
+        }
     }
+
+    // If some of the above autoload checks were successful, we should have a TYPO3_COMPOSER_MODE env.
+    if (TYPO3_COMPOSER_MODE !== true) {
+        throw new RuntimeException('Unable to find composer vendor dir! Autoload failed.');
+    }
+
 
     $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
         ->getQueryBuilderForTable('tt_content');
