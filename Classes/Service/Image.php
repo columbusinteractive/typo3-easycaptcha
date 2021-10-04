677cfd2bea2517da7bef62c9e3362e9b175aebdd
Void <57257404+into-the-v0id@users.noreply.github.com>
2021-10-04 13:56:42 +0200

    Moved composer autoloading into separate file

--- Classes/Service/Image.php
+++ Classes/Service/Image.php
@@ -6,8 +6,6 @@ use ColumbusInteractive\EasyCaptcha\Exceptions\MissingFormElement;
 use ColumbusInteractive\EasyCaptcha\Exceptions\UnableToLoadFormConfiguration;
 use ColumbusInteractive\EasyCaptcha\Service\Captcha;
 use TYPO3\CMS\Core\Configuration\Loader\YamlFileLoader;
-use TYPO3\CMS\Core\Core\Bootstrap;
-use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
 use TYPO3\CMS\Core\Database\Query\Restriction\FrontendWorkspaceRestriction;
@@ -15,33 +13,7 @@ use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 (static function () {
-
-    // Default vendor dir
-    if (file_exists(dirname(__DIR__, 6) . '/vendor/autoload.php')) {
-        // Composer mode
-        $classLoader = require dirname(__DIR__, 6) . '/vendor/autoload.php';
-        SystemEnvironmentBuilder::run(5, SystemEnvironmentBuilder::REQUESTTYPE_FE);
-        Bootstrap::init($classLoader);
-
-    // Custom vendor dir
-    } else if (isset($_SERVER['DOCUMENT_ROOT']) &&
-        file_exists(dirname($_SERVER['DOCUMENT_ROOT'], 1) . '/composer.json')) {
-        $composerConfig = json_decode(file_get_contents(
-            dirname($_SERVER['DOCUMENT_ROOT'], 1) . '/composer.json'
-        ), true);
-
-        if (isset($composerConfig['config']['vendor-dir'])) {
-            $classLoader = require dirname(__DIR__, 6) . '/' . $composerConfig['config']['vendor-dir'] . '/autoload.php';
-            SystemEnvironmentBuilder::run(5, SystemEnvironmentBuilder::REQUESTTYPE_FE);
-            Bootstrap::init($classLoader);
-        }
-    }
-
-    // If some of the above autoload checks were successful, we should have a TYPO3_COMPOSER_MODE env.
-    if (TYPO3_COMPOSER_MODE !== true) {
-        throw new RuntimeException('Unable to find composer vendor dir! Autoload failed.');
-    }
-
+    require '../../composer_autoload.php';
 
     $context = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Context\Context::class);
     assert($context instanceof \TYPO3\CMS\Core\Context\Context);
